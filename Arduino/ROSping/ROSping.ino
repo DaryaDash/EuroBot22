a152c60a1e94db50149114a491db01b668a4dac6
Даша Дашкова <dashkova74@gmail.com>
2022-03-03 18:32:26 +0800

    ping edit

--- Arduino/ROSping/ROSping.ino
+++ Arduino/ROSping/ROSping.ino
@@ -1,13 +1,14 @@
 #include <ros.h>
 #include <sensor_msgs/Range.h>
-#define PIN_ECHOL 42
 #define PIN_TRIGL 43
+#define PIN_ECHOL 42
 
-#define PIN_ECHOR 46
 #define PIN_TRIGR 47
+#define PIN_ECHOR 46
 
-#define PIN_ECHOF 44
 #define PIN_TRIGF 45
+#define PIN_ECHOF 44
+long duration_R,duration_L, duration_F, cm_R, cm_L, cm_F;
 
 ros::NodeHandle nh;
 sensor_msgs::Range rangeR_msg;
@@ -17,8 +18,6 @@ ros::Publisher pub_range_left ("range_left", &rangeL_msg);
 ros::Publisher pub_range_right ("range_right", &rangeR_msg);
 ros::Publisher pub_range_forward ("range_forward", &rangeF_msg);
 
-long duration_right,duration_left,duration_forward, cm_right,cm_left,cm_forward;
-
 void setup() {
   nh.initNode();
   nh.advertise(pub_range_left);
@@ -26,24 +25,23 @@ void setup() {
   nh.advertise(pub_range_forward);
   Serial.begin (9600);
   //Определяем вводы и выводы
-  pinMode(PIN_TRIGL, OUTPUT);
-  pinMode(PIN_ECHOL, INPUT);
-   pinMode(PIN_TRIGR, OUTPUT);
-  pinMode(PIN_ECHOR, INPUT);
-   pinMode(PIN_TRIGF, OUTPUT);
-  pinMode(PIN_ECHOF, INPUT);
-  // put your
+ pinMode(PIN_TRIGR, OUTPUT);
+ pinMode(PIN_ECHOR, INPUT);
+ pinMode(PIN_TRIGL, OUTPUT);
+ pinMode(PIN_ECHOL, INPUT);
+ pinMode(PIN_TRIGF, OUTPUT);
+ pinMode(PIN_ECHOF, INPUT);
 }
 
 void loop() {
+  digitalWrite(PIN_TRIGL, LOW);
   digitalWrite(PIN_TRIGF, LOW);
   digitalWrite(PIN_TRIGR, LOW);
-  digitalWrite(PIN_TRIGL, LOW);
-  delayMicroseconds(5);
-  digitalWrite(PIN_TRIGL, HIGH);
-  digitalWrite(PIN_TRIGR, HIGH);
-  digitalWrite(PIN_TRIGF, HIGH);
-
+ delayMicroseconds(2);
+  //  Время задержки акустического сигнала на эхолокаторе.
+  duration_R = pulseIn(PIN_ECHOR, HIGH);
+  duration_F = pulseIn(PIN_ECHOF, HIGH);
+  duration_L = pulseIn(PIN_ECHOL, HIGH);
   // Выставив высокий уровень сигнала, ждем около 10 микросекунд. В этот момент датчик будет посылать сигналы с частотой 40 КГц.
   delayMicroseconds(10);
   digitalWrite(PIN_TRIGL, LOW);
@@ -56,22 +54,13 @@ void loop() {
   duration_forward = pulseIn(PIN_ECHOF, HIGH);
 
   // Теперь осталось преобразовать время в расстояние
-  cm_right = (duration_right / 2) / 29.1; 
-  cm_left=( duration_left/ 2)/ 29.1;
-  cm_forward=(duration_forward/ 2)/ 29.1; // Сначала генерируем короткий импульс длительностью 2-5 микросекунд
- Serial.print("distance R: ");
-  Serial.print(cm_right);
-  Serial.println("cm.");
-  Serial.print("distance L: ");
-  Serial.print(cm_left);
-  Serial.println("cm.");
-  Serial.print("distance F: ");
-  Serial.print(cm_forward);
-  Serial.println("cm.");
-  //*/
-  rangeR_msg.range=cm_right;
-  rangeL_msg.range=cm_left;
-  rangeF_msg.range=cm_forward;
+ cm_R = (duration_R / 2) / 29.1;  // Сначала генерируем короткий импульс длительностью 2-5 микросекунд.
+ cm_F = (duration_F / 2) / 29.1;
+ cm_L = (duration_L / 2) / 29.1;
+ 
+  rangeR_msg.range=cm_R;
+  rangeL_msg.range=cm_L;
+  rangeF_msg.range=cm_F;
   pub_range_left.publish (&rangeL_msg);
   pub_range_right.publish (&rangeR_msg);
   pub_range_forward.publish (&rangeF_msg);
--- Arduino/hc-sr04/hc-sr04.ino
+++ Arduino/hc-sr04/hc-sr04.ino
@@ -1,7 +1,9 @@
 #define PIN_TRIGL 43
 #define PIN_ECHOL 42
-#define PIN_TRIGR 45
-#define PIN_ECHOR 44
+
+#define PIN_TRIGR 47
+#define PIN_ECHOR 46
+
 #define PIN_TRIGF 45
 #define PIN_ECHOF 44
 long duration_R,duration_L, duration_F, cm_R, cm_L, cm_F;
@@ -12,12 +14,12 @@ void setup() {
 
   Serial.begin (9600);
   //Определяем вводы и выводы
-  pinMode(PIN_TRIGR, OUTPUT);
-  pinMode(PIN_ECHOR, INPUT);
-   pinMode(PIN_TRIGL, OUTPUT);
-  pinMode(PIN_ECHOL, INPUT);
-   pinMode(PIN_TRIGF, OUTPUT);
-  pinMode(PIN_ECHOF, INPUT);
+ pinMode(PIN_TRIGR, OUTPUT);
+ pinMode(PIN_ECHOR, INPUT);
+ pinMode(PIN_TRIGL, OUTPUT);
+ pinMode(PIN_ECHOL, INPUT);
+ pinMode(PIN_TRIGF, OUTPUT);
+ pinMode(PIN_ECHOF, INPUT);
 }
 
 void loop() {
@@ -28,7 +30,7 @@ void loop() {
   delayMicroseconds(5);
   digitalWrite(PIN_TRIGR, HIGH);
   digitalWrite(PIN_TRIGF, HIGH);
-    digitalWrite(PIN_TRIGL, HIGH);
+  digitalWrite(PIN_TRIGL, HIGH);
 
   // Выставив высокий уровень сигнала, ждем около 10 микросекунд. В этот момент датчик будет посылать сигналы с частотой 40 КГц.
   delayMicroseconds(10);
@@ -38,9 +40,9 @@ void loop() {
   //  Время задержки акустического сигнала на эхолокаторе.
   duration_R = pulseIn(PIN_ECHOR, HIGH);
   duration_F = pulseIn(PIN_ECHOF, HIGH);
-    duration_L = pulseIn(PIN_ECHOL, HIGH);
+  duration_L = pulseIn(PIN_ECHOL, HIGH);
   // Теперь осталось преобразовать время в расстояние
-  cm_R = (duration_R / 2) / 29.1;  // Сначала генерируем короткий импульс длительностью 2-5 микросекунд.
+ cm_R = (duration_R / 2) / 29.1;  // Сначала генерируем короткий импульс длительностью 2-5 микросекунд.
  cm_F = (duration_F / 2) / 29.1;
  cm_L = (duration_L / 2) / 29.1;
   Serial.print("R: ");
@@ -49,7 +51,7 @@ void loop() {
   Serial.print("F: ");
   Serial.print(cm_F);
   Serial.println("cm.");
-   Serial.print("L: ");
+  Serial.print("L: ");
   Serial.print(cm_L);
   Serial.println("cm.");
 
