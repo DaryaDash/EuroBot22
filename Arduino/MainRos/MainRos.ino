cf68781666a2e6fd86e1591e8b426e73b7ad6551
Арсений Кузнецов <dashkova74@gmail.com>
2022-03-14 18:35:25 +0700

    n-code

--- Arduino/MainRos/MainRos.ino
+++ Arduino/MainRos/MainRos.ino
@@ -16,6 +16,8 @@ int posL=0;
 int posR=0;
 int posB=0;
 
+ros::NodeHandle nh;
+
 int MotorSpeed []={3,4,5}; //Right,Back,Left
 int MotorDirectionRight [] = {22,25,29};//Right,Back,Left
 int MotorDirectionLeft [] = {24,28,27};//Right,Back,Left
@@ -43,7 +45,6 @@ volatile float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f; // quaternion of sens
 
 float angles[3];
 
-ros::NodeHandle nh;
 
 
 sensor_msgs::Range rangeR_msg;
--- Arduino/n-code/n-code.ino
+++ Arduino/n-code/n-code.ino
@@ -1,76 +1,123 @@
 
-#define INT_A_PIN 2
+#include <ros.h>
+#include <std_msgs/Float64.h>
+#define PIN_ENA 2 // Вывод управления скоростью вращения мотора л
+#define PIN_ENB 3   // Вывод управления скоростью вращения мотора №2п
+#define PIN_ENC 4 // Вывод управления скоростью вращения мотора №3з
+#define PIN_IN1 26 // Вывод управления направлением вращения мотора №1л
+#define PIN_IN2 27 // Вывод управления направлением вращения мотора №1л
+#define PIN_IN3 28 // Вывод управления направлением вращения мотора №2п
+#define PIN_IN4 29 // Вывод управления направлением вращения мотора №2п
+#define PIN_IN5 22 // Вывод управления направлением вращения мотора №3
+#define PIN_IN6 25 // Вывод управления направлением вращения мотора №3
 
-#define INT_B_PIN 3
+ros::NodeHandle nh;
 
-#define SIG_A_PIN 4
+int ENCA []={49,51,53}; /// left, back, right
+int ENCB []={48,50,52}; /// left, back, right
+int posL=0;
+int posR=0;
+int posB=0;
+]std_msgs::Float64 EnR_msg;
+std_msgs::Float64 EnL_msg;
+std_msgs::Float64 EnB_msg;
 
-#define SIG_B_PIN 5
+ros::Publisher pub_EnR ("range_left_enc", &EnR_msg);
+ros::Publisher pub_EnL ("range_right_enc", &EnL_msg);
+ros::Publisher pub_EnB ("range_front_enc", &EnB_msg);
 
-long encA = 0;
-
-long encB = 0;
+void readEncoder(){
+  int b = digitalRead(ENCB[1]);
+  int r = digitalRead(ENCB[2]);
+  int l = digitalRead(ENCB[0]);
+  if(b > 0){
+    posB++;
+    
+  pub_range_front.publish (&EnB_msg);
+  }
+  else{
+     if (b=0) {
+       posB=posB;
+       Serial.print(posB);
+  Serial.println();
+  }
+  else {posB--;
+  Serial.print(posB);
+  Serial.println();
+  }
+    if(l > 0){
+    posL++;
+    Serial.print(posL);
+   Serial.println();
+  }
+  else{
+       if (l=0) {
+       posL=posL;
+       Serial.print(posL);
+   Serial.println();
+  }
+  else {posL--;
+  Serial.print(posL);
+   Serial.println();
+  }
+  }
+    if(r > 0){
+    posR++;
+    Serial.print(posR);
+  Serial.println();
+  }
+  else{
+       if (r=0) {
+  Serial.print(posR);
+  Serial.println();
+  }
+  else {posR--;
+  Serial.print(posR);
+  Serial.println();}
+  }
+}
+}
 
 void setup() {
+nh.initNode();
+nh.advertise(pub_EnR);
+nh.advertise(pub_EnL);
+nh.advertise(pub_EnB);
+ for(int i=0; i < 3; i++) {
+attachInterrupt(digitalPinToInterrupt( ENCA[i]), readEncoder, RISING);
 
-pinMode(INT_A_PIN, INPUT);
+attachInterrupt(digitalPinToInterrupt(ENCB[i]), readEncoder, RISING);
+pinMode(ENCA[i], INPUT);
+pinMode(ENCB[i], INPUT);
+  }
+    pinMode(PIN_ENA, OUTPUT);
+  pinMode(PIN_ENB, OUTPUT);
+  pinMode(PIN_ENC, OUTPUT);
+  pinMode(PIN_IN1, OUTPUT);
+  pinMode(PIN_IN2, OUTPUT);
+  pinMode(PIN_IN3, OUTPUT);
+  pinMode(PIN_IN4, OUTPUT);
+  pinMode(PIN_IN5, OUTPUT);
+  pinMode(PIN_IN6, OUTPUT);
+ analogWrite(PIN_ENA, 160);
+   analogWrite(PIN_ENC, 160); // Устанавливаем скорость 1-го мотора
+   analogWrite(PIN_ENB, 160
+  ); // Устанавливаем скорость 1-го мотора
+    // Задаём направление для 1-го мотора
+    digitalWrite(PIN_IN1, LOW);
+    digitalWrite(PIN_IN2, HIGH);
+    // Задаём направление для 2-го мотора
+   digitalWrite(PIN_IN3, LOW);
+    digitalWrite(PIN_IN4,HIGH);
+       // Задаём направление для 3-го мотора
+    digitalWrite(PIN_IN5, HIGH);
+    digitalWrite(PIN_IN6, LOW);
 
-pinMode(INT_B_PIN, INPUT);
+  }
 
-pinMode(SIG_A_PIN, INPUT);
-
-pinMode(SIG_B_PIN, INPUT);
-
-Serial.begin(9600);
-
-attachInterrupt(digitalPinToInterrupt(INT_A_PIN), EncA, RISING);
-
-attachInterrupt(digitalPinToInterrupt(INT_B_PIN), EncB, RISING);
-
-}
-
-void EncA()
-
-{
-
-if ( digitalRead(SIG_A_PIN) )
-
-{
-
-encA++;
-
-} else {
-
-encA--;
-
-}
-
-}
-
-void EncB()
-
-{
-
-if ( digitalRead(SIG_A_PIN) )
-
-{
-
-encB--;
-
-} else {
-
-encB++;
-
-}
-
-}
 
 void loop() {
-
-Serial.print(encA);
-
-Serial.print("\t");
-
-Serial.println(encB);
+   readEncoder;
+  // put your main code here, to run repeatedly:
 
 }
