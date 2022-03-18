b12f8ee62e0c9fce46a8f7c3e65ffccaeb700019
Арсений Кузнецов <dashkova74@gmail.com>
2022-03-18 18:27:56 +0700

    add header

--- Arduino/n-code/n-code.ino
+++ Arduino/n-code/n-code.ino
@@ -1,10 +1,23 @@
 
-#define ENCA 49
+#include <PinChangeInterrupt.h>
+#define ENCA 51
 
-#define ENCB 48
+#define ENCB 50
 
 
 int pos = 0;
+
+void readEncoder(){
+  Serial.println("work");
+  int b = digitalRead(ENCB);
+  if(b > 0){
+    pos++;
+  }
+  else{
+    pos--;
+  }
+                     }
+ 
 
 void setup() {
   Serial.begin(9600);
@@ -16,30 +29,20 @@ void setup() {
 
 
 void loop() {
-  readEncoder;
-  Serial.print(pos);
+}
   //Serial.println(pos);
-//Serial.print("A");
- //Serial.print(digitalRead(ENCA)*5);
- //Serial.print(" B");
- //Serial.println(digitalRead(ENCB)*5);
+/*Serial.print("A");
+ Serial.print(digitalRead(ENCA)*5);
+ Serial.print(" B");
+ Serial.println(digitalRead(ENCB)*5);
  /*if (digitalRead(ENCA)>0){
    pos ++;
  }
  if (digitalRead(ENCB)>0){
    pos--;
  }
- Serial.println(pos);*/
- delay(10);
-            }
+ Serial.println(pos);
+ }*/
+            
 
-void readEncoder(){
-  int b = digitalRead(ENCB);
-  if(b > 0){
-    pos++;
-  }
-  else{
-    pos--;
-  }
-                     }
- 
+
