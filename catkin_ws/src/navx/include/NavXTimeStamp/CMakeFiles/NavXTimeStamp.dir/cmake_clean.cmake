8e3a1a1563171923073511137992e9ef55060770
Dmitriy <dashkova74@gmail.com>
2022-03-30 17:28:57 +0700

    rosNav

---
+++ catkin_ws/src/navx/include/NavXTimeStamp/CMakeFiles/NavXTimeStamp.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NavXTimeStamp.dir/AHRS.cpp.o"
+  "CMakeFiles/NavXTimeStamp.dir/ContinuousAngleTracker.cpp.o"
+  "CMakeFiles/NavXTimeStamp.dir/InertialDataIntegrator.cpp.o"
+  "CMakeFiles/NavXTimeStamp.dir/OffsetTracker.cpp.o"
+  "CMakeFiles/NavXTimeStamp.dir/SerialIO.cpp.o"
+  "CMakeFiles/NavXTimeStamp.dir/SerialPort.cpp.o"
+  "CMakeFiles/NavXTimeStamp.dir/TimeStamp.cpp.o"
+  "NavXTimeStamp"
+  "NavXTimeStamp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NavXTimeStamp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
